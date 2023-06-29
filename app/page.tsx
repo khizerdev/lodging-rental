d57cb6340adb34fb0d8090cb2d3c18f492578b1c
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-06-29 22:45:04 +0500

    Listing fetched using server components

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
         <div 
           className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {
-            listings.map((item: any) => {
+            listings.map((item) => {
               return (
                 <ListingCard
                   key={item.id}
