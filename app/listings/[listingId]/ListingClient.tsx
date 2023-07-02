3aacbfe45dce0200ae016f66a46a6eaaca575c82
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-07-02 23:45:20 +0500

    Date range work added

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import { useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
+import { Range } from "react-date-range";
 import { categoriesList } from "@/app/data/categoriesList";
 
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
 
+import { differenceInDays, eachDayOfInterval } from 'date-fns';
+
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+};
 
 
 interface ListingClientProps {
@@ -24,11 +36,33 @@ const ListingClient: React.FC<ListingClientProps> = ({
   reservations = [],
   currentUser
 }) => {
+
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const disabledDates = useMemo(() => {
+        let dates: Date[] = [];
+
+        reservations.forEach((reservation: any) => {
+            const range = eachDayOfInterval({
+            start: new Date(reservation.startDate),
+            end: new Date(reservation.endDate)
+            });
+
+            dates = [...dates, ...range];
+        });
+
+        return dates;
+    }, [reservations]);
   
     const category = useMemo(() => {
     return categoriesList.find((items) => 
         items.label === listing.category);
     }, [listing.category]);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(listing.price);
+    const [dateRange, setDateRange] = useState<Range>(initialDateRange);
 
     return ( 
         <Container>
